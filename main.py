85ab11146e2e2dc987b58356ec134dd7ca808f67
[MJRaymundo] <[m4rkjohnraymundo+github@gmail.com]>
2023-02-17 15:33:36 +0800

    Uploaded Files to be Modifiedd

---
+++ main.py
@@ -0,0 +1,13 @@
+# Mark john M. Raymundo CoE 2-2 Data Structure and Algorithms
+# Modified Text Editor Project by github user: iampiyushjain
+# Source Link: https://github.com/iampiyushjain/Text-Editor
+# Modified Parts : Windows.py lines:
+
+from Window import *
+
+TextEditor = Window()
+
+TextEditor.TextBox.pack(expand=1, fill="both")
+TextEditor.window.protocol("WM_DELETE_WINDOW", TextEditor.on_closing)
+TextEditor.window.bind("<Key>", TextEditor.key_pressed)
+TextEditor.window.mainloop()
